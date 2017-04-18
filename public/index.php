88fb57c27adafcd42a7adaad1ccadf94ae6ec0cd
Cayden <cayden.simler@hotmail.com>
2017-04-18 15:45:02 -0500

    Added images and a central JS folder. Finished the homepage to incorprate a few featured listings chosen at random.

--- public/index.php
+++ public/index.php
@@ -6,14 +6,14 @@ require_once __DIR__ . '/../bootstrap.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <title>OooLister</title>
+    <title>JJC - Adlister</title>
     <?php require '../views/partials/head.php'; ?>
 </head>
 <body>
     <?php require '../views/partials/navbar.php'; ?>
 
-    <?php require $mainView; ?>
-
+    <?php  require $mainView; ?>
+    
     <?php require '../views/partials/common_js.php'; ?>
 </body>
 </html>
