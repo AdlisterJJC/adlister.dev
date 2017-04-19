2023c3c76e0fa744008da3e77f67352a554c6544
Joshua Williams <joshuamw322@gmail.com>
2017-04-19 15:01:28 -0500

    created userInsert function on User model

--- models/adlister_test.php
+++ models/adlister_test.php
@@ -1,9 +1,18 @@
 <?php  
 // require_once 'Model.php';
 require_once 'Ads.php';
+require_once 'User.php';
 
-var_dump(Ads::all());
+// var_dump(Ads::all());
+
+$user = new User();
+
+$user->name = 'Billy?';
+$user->email = 'makingaliving@livingemail.com';
+$user->username = 'arewedoneyet';
+$user->password = 'eighttosixteen';
 
 
+$user->insertUser();
 
 ?>
