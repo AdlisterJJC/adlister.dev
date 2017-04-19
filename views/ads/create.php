569288a0c20dbbcd924a098486696c9a7bc5c7c1
Cayden <cayden.simler@hotmail.com>
2017-04-19 15:21:57 -0500

    Added the form for creating an ad. Also added a sample image for the ads on the /results page.

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,1 +1,42 @@
 <!--Page for creating new advertisement listings-->
+
+
+<div class="col-xs-12 invisible invisibleFour">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At sequi repellat reprehenderit suscipit, aliquam et obcaecati.</div>
+
+<div class="col-xs-12 createTitle">
+	Create a Listing
+</div>
+
+<div class="col-xs-12 invisible">filler text</div>
+
+<div class="col-xs-offset-2 col-xs-8 createBody">
+	<form action="" method="POST">
+		<div class="col-xs-12 invisible">filler text</div>
+		<div class="col-xs-7 createHeaders">Title</div>
+        <div class="col-xs-offset-1 col-xs-3 createHeaders">Price</div>
+            <div class="col-xs-7">
+                <input type="text" name="createTitle" id="createTitle" class="loginSignupInput">
+            </div>
+        	<div class="col-xs-offset-1 col-xs-4">
+        	  	<input type="text" name="createPrice" id="createPrice" class="loginSignupInput">
+			</div>
+        <div class="col-xs-12 invisible invisibleTwo">filler text</div>
+        <div class="col-xs-12 createHeaders">Summary</div>
+        	<div class="col-xs-12">
+                <input type="text" name="createSummary" id="createSummary" class="loginSignupInput">
+            </div>
+        <div class="col-xs-12 invisible invisibleTwo">filler text</div>
+        <div class="col-xs-12 createHeaders">Description</div>
+        	<div class="col-xs-12">
+                <textarea name="createDescription" id="createDescription" class="createDescription"></textarea>
+            </div>
+        <div class="col-xs-12 invisible invisibleTwo">filler text</div>
+        <div class="col-xs-12 createHeaders">Categories</div>
+        	<div class="col-xs-12">
+                <input type="text" name="createCategories" id="createCategories" class="loginSignupInput">
+            </div>
+        <div class="col-xs-12 invisible invisibleTwo">filler text</div>
+	</form>
+</div>
+
+<div class="col-xs-12 invisible invisibleFour">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At sequi repellat reprehenderit suscipit, aliquam et obcaecati.</div>
--- views/ads/index.php
+++ views/ads/index.php
@@ -11,9 +11,11 @@ $ads = Ads::all();
 <div class="col-xs-12 invisible invisibleFour">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At sequi repellat reprehenderit suscipit, aliquam et obcaecati.</div>
 
 <?php foreach ($ads as $ad): ?>
-	<?=  '<div class="col-xs-offset-1 col-xs-10 col-sm-4">'?>
+	<?=  '<div class="col-xs-12 col-sm-4">'?>
 		<?= '<div class="col-xs-12 title">' . $ad->item . ' - $' . $ad->price . '</div>' ?>
+		<?= '<div class="col-xs-12"><img src="img/sample.png"></div>' ?>
 		<?= '<div class="col-xs-12 summary">' . $ad->summary . '</div>' ?>
+		<?= '<div class="col-xs-12 invisible">Lorem</div>' ?>
 	<?= '</div>' ?>
 <?php endforeach; ?>
 
