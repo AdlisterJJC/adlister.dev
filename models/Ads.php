4c146a1762f3ae4e9b1e92d907ab7db13c5ca4ab
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-04-19 10:39:43 -0500

    defined findAdByKeyword method in Ads model.

--- models/Ads.php
+++ models/Ads.php
@@ -5,14 +5,27 @@ require_once __DIR__ . '/Model.php';
 class Ads extends Model {
 	protected static $table = 'ads';
 
+	public static function findAdByKeyword($keyword){
+		// connect to database;
+		self::dbConnect();
 
-	
+		$query = 'SELECT * FROM' . self::$table . ' WHERE item LIKE %:item%';
 
+		$stmt = self::$dbc->prepare($query);
+		$stmt->bindValue(':item', $keyword, PDO::PARAM_STR);
+		$stmt->execute();
+
+		$results = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		$instance = null;
+		if($results) {
+			$instance = new static;
+			$instance->attributes = $results;
+		}
+
+		return $instance;
+
+	}
 
 }
-
-
-
-
-
 ?>
