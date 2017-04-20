00abd9855278714f7fb4e7bada224d908bafaae0
Cayden <cayden.simler@hotmail.com>
2017-04-20 14:37:26 -0500

    Removed the account page link if not signed in.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -38,16 +38,15 @@
                 <li class="hidden">
                     <a href="#page-top"></a>
                 </li>
-
-                <li>
-                    <a href="/users/account">Account</a>
-                </li>
-                <?php if (!isset($_SESSION['username'])) { ?>
+                <?php if (!isset($_SESSION['IS_LOGGED_IN'])) { ?>
                 <?= '<li>
                         <a href="/login">Login/Signup</a>
                     </li>'; ?> 
-                <?php } else if (isset($_SESSION['username'])) { ?>
+                <?php } else if (isset($_SESSION['IS_LOGGED_IN'])) { ?>
                 <?= '<li>
+                        <a href="/users/account">Account</a>
+                    </li>
+                    <li>
                         <a href="/logout">Logout</a>
                     </li>'; }?>
             </ul>
