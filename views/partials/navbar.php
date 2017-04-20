153550956df9bbe83c832c364c7f5d340d76c2e0
Cayden <cayden.simler@hotmail.com>
2017-04-20 15:09:20 -0500

    .

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -9,7 +9,7 @@
                 <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
             </button>
         </div>
-
+        
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-left">
