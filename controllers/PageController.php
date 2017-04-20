17cc6d69411949b9f382e71f13d612f3415a774c
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-04-20 12:03:55 -0500

    Added authentication check and login rules for login form in case /login

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -8,51 +8,75 @@ require_once __DIR__ . '/../utils/helper_functions.php';
 function pageController()
 {
 
-    function loginCheck() {
-        
-    }
+	function loginCheck() {
 
-    // defines array to be returned and extracted for view
-    $data = [];
+		if(!empty($_POST)) {
+			$usernameOrEmail = Input::get('loginUsername');
+			$password = Input::get('password');
+			Auth::attempt($usernameOrEmail, $password);
+			if (Auth::check()) {
+				header("Location: http://adlister.dev/account.php");
+				exit;
+			} else {
+				$message = "Please retry username/email or password.";
+			}
+	}
+		
+	}
 
-    // get the part of the request after the domain name
-    $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	// defines array to be returned and extracted for view
+	$data = [];
 
-    // switch that will run functions and setup variables dependent on what route was accessed
-    switch ($request) {
-        // TODO: put routes here
-        case '/':
-            $mainView = '../views/home.php';
-            break;
-        case '/users/edit':
-            $mainView = '../views/users/edit.php';
-            break;
-        case '/users/account':
-            $mainView = '../views/users/account.php';
-            break;
-        case '/results':
-            $mainView = '../views/ads/index.php';
-            break;
-        case '/login':
-            $mainView = '../views/users/login.php';
-            break;
-        case '/signup':
-            $mainView = '../views/users/signup.php';
-            break;
-        case '/create':
-            $mainView = '../views/ads/create.php';
-            break;
-        case '/edit':
-            $mainView = '../views/ads/edit.php';
-            break;
-        default:    // displays 404 if route not specified above
-            $mainView = '../views/404.php';
-            break;
-    }
+	// get the part of the request after the domain name
+	$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    $data['mainView'] = $mainView;
+	// switch that will run functions and setup variables dependent on what route was accessed
+	switch ($request) {
+		// TODO: put routes here
+		case '/':
+			$mainView = '../views/home.php';
+			break;
+		case '/users/edit':
+			$mainView = '../views/users/edit.php';
+			break;
+		case '/users/account':
+			$mainView = '../views/users/account.php';
+			break;
+		case '/results':
+			$mainView = '../views/ads/index.php';
+			break;
+		case '/login':
+			$mainView = '../views/users/login.php';
+			$data['message'] = '';
 
-    return $data;
+			if(Auth::check()){
+				header("Location: http://adlister.dev/account.php");
+			}
+			if(!empty($_POST)) {
+				$usernameOrEmail = Input::get('loginUsername');
+				$password = Input::get('loginPassword');
+				Auth::attempt($usernameOrEmail, $password);
+			} else {
+				$data['message'] = "Please enter username or password.";
+			}
+			break;
+		case '/signup':
+			$mainView = '../views/users/signup.php';
+			break;
+		case '/create':
+			$mainView = '../views/ads/create.php';
+			break;
+		case '/edit':
+			$mainView = '../views/ads/edit.php';
+			break;
+		default:    // displays 404 if route not specified above
+			$mainView = '../views/404.php';
+			break;
+	}
+
+	$data['mainView'] = $mainView;
+
+	return $data;
 }
 
 extract(pageController());
